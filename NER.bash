c3c9f4f2f5c4220a6bac8a7d24d20c22d14037b6
Ling Sun <ls44@iu.edu>
2024-12-10 03:08:09 -0500

    Update NER.bash

--- NER.bash
+++ NER.bash
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#SBATCH --account r00213
 #SBATCH -p general
 #SBATCH --nodes=12
 #SBATCH --ntasks-per-node=8
 #SBATCH -o base_out_%j.out
 #SBATCH -e base_error_%j.err
-#SBATCH --mail-user=ls44@iu.edu
 #SBATCH --mail-type=ALL
 #SBATCH --time=2-30:00:00
 #SBATCH --job-name=NER_processor
